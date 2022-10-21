2cb5bd7eaee25b5fb9262502c388e1c7c4bf56a2
DarkMatter-999 <darkmatter999official@gmail.com>
2022-10-21 11:10:35 +0530

    backend: Added update password, friend and unfriend functionality

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -59,12 +59,52 @@ export const updateUser = async (req, res) => {
     }
 }
 
+export const updatePasswd = async (req, res) => {
+    const id = req.params.id
+
+    const { id_, passwd, newpasswd } = req.body
+
+    if(id_ === id) {
+        try {
+            if(passwd) {
+                const user = await UserModel.findById(id_)
+                
+                const valid = await bcrypt.compare(passwd, user.passwd)
+
+                const salt = await bcrypt.genSalt(10)
+                req.body.passwd = await bcrypt.hash(newpasswd, salt)
+
+                if(!valid) {
+                    res.status(400).json("Could not update User")
+                } else {
+                    const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true}).select("-passwd")
+
+                    const token = jwt.sign({
+                        username: user.username,
+                        id: user._id,
+                    },
+                    process.env.TOKEN_SECRET,
+                    {
+                        expiresIn: '10h'
+                    })
+
+                    res.status(200).json({user, token})
+                }
+            }
+        } catch (error) {
+            res.status(500).json({message: error.message})
+        }
+    } else {
+        res.status(403).json("Access Denied")
+    }
+}
+
 export const deleteUser = async (req, res) => {
     const id = req.params.id
 
-    const { id_, username, passwd } = req.body
-    console.log(req.body);
-    if(id_ === id) {
+    const { _id, passwd } = req.body
+
+    if(_id === id) {
         try {
             if(passwd) {
                 const user = await UserModel.findById(id_)
@@ -88,9 +128,43 @@ export const deleteUser = async (req, res) => {
 }
 
 export const friendUser = async (req, res) => {
+    const id = req.params.id
 
+    const {_id} = req.body
+
+    if(id===_id) {
+        res.status(403).json("Action forbidden")
+    } else {
+        const self = await UserModel.findById(_id)
+        const friend = await UserModel.findById(id)
+
+        if(!self.friends.includes(id)) {
+            await self.updateOne({$push: {friends: id}})
+            await friend.updateOne({$push: {friends: _id}})
+            res.status(200).json("Friended user")
+        } else {
+            res.status(403).json("Already friend")
+        }
+    }
 }
 
 export const unfriendUser = async (req, res) => {
+    const id = req.params.id
 
+    const {_id} = req.body
+
+    if(id===_id) {
+        res.status(403).json("Action forbidden")
+    } else {
+        const self = await UserModel.findById(_id)
+        const friend = await UserModel.findById(id)
+
+        if(self.friends.includes(id)) {
+            await self.updateOne({$pull: {friends: id}})
+            await friend.updateOne({$pull: {friends: _id}})
+            res.status(200).json("Unfriended user")
+        } else {
+            res.status(403).json("Already not friend")
+        }
+    }
 }
--- backend/Routes/User.js
+++ backend/Routes/User.js
@@ -1,12 +1,14 @@
 import express from 'express'
-import { deleteUser, getUser, updateUser } from '../Controllers/UserController.js'
+import { deleteUser, friendUser, getUser, unfriendUser, updatePasswd, updateUser } from '../Controllers/UserController.js'
 
 const router = express.Router()
 
 router.get('/:id', getUser)
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
-// router.put('/:id/friend', friendUser)
-// router.put('/:id/unfriend', unfriendUser)
+
+router.put('/:id/updatepw', updatePasswd)
+router.put('/:id/friend', friendUser)
+router.put('/:id/unfriend', unfriendUser)
 
 export default router
